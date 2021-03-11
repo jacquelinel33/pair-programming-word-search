ced1fc797c14c1d0c795fd4b93508d31cdfbcef3
Jacqueline <jlee4332@gmail.com>
2021-03-10 23:45:57 -0500

    adding refactor code

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,38 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (inputArray, givenWord) => {
+  if (inputArray.length === 0) {
+    return 'you\'ve inputted an empty array';
+  }
+  let transposedArray = transpose(inputArray);
+  const horizontalWords = inputArray.map(ls => ls.join(''));
+  for (let word of horizontalWords) {
+    if (word.includes(givenWord)) {
+      return true;
     }
-}
-
-module.exports = wordSearch
+  }
+  const verticalWords = transposedArray.map(ls => ls.join(''));
+  // console.log(transposedArray)
+  for (let word of verticalWords) {
+    if (word.includes(givenWord)) {
+      return true;
+    }
+  }
+  return false;
+  
+};
+  
+const transpose = function(matrix) {
+  let newArray = [];
+  for (let r in matrix[0]) {
+    newArray.push([]);
+  }
+  for (let r = 0; r < matrix.length; r++) {
+    for (let c = 0; c < matrix[r].length; c++) {
+      newArray[c][r] = matrix[r][c];
+    }
+  } return newArray;
+  
+};
+  
+console.log(wordSearch([], 'SEINFELD'));
+  
+module.exports = wordSearch;
